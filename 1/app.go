a71779cbdca086643b33d6e2fc41762011efc116
Timothy Andrew <mail@timothyandrew.net>
2018-12-02 23:34:05 +0530

    1

--- 1/app.go
+++ 1/app.go
@@ -7,6 +7,18 @@ import (
 	"strings"
 )
 
+type Set struct {
+	m map[int]bool
+}
+
+func (s *Set) add(n int) {
+	s.m[n] = true
+}
+
+func (s Set) exists(n int) bool {
+	return (s.m[n] == true)
+}
+
 func readFile(filename string) string {
 	data, _ := ioutil.ReadFile(filename)
 	return string(data)
@@ -16,10 +28,21 @@ func main() {
 	input := strings.Split(readFile("input.txt"), "\n")
 
 	sum := 0
+	seen := Set{make(map[int]bool)}
+	seen.add(0)
 
-	for _, line := range input {
-		number, _ := strconv.Atoi(line)
-		sum += number
+	for {
+		for _, line := range input {
+			number, _ := strconv.Atoi(line)
+			sum += number
+
+			if seen.exists(sum) {
+				fmt.Println("The first repeat was", sum)
+				return
+			}
+
+			seen.add(sum)
+		}
 	}
 
 	fmt.Println(sum)
